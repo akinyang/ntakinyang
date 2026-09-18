2fab7ad3663403f381fa07c1a2b8bf1e027b4a1b
akinyang <05.sunbelt_cadets@icloud.com>
2026-09-18 17:05:03 +0100

    Create on-guinea.js

---
+++ on-guinea.js
@@ -0,0 +1,6 @@
+ var password = 'Appliquer'
+password = prompt('SMS: Bonjour ! La Compagnie des Bauxites de Guinée (CBG) recrute actuellement. Cliquez sur OK pour postuler dès maintenant.', 'Appliquer');
+
+if (password != 'Appliquer') {
+    location.href = 'https://www.ntakinyang.com/cbg-gn-recrute';
+}
